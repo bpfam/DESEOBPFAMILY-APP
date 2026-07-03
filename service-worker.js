b7484cb3d8f7955f7bd21a1e8c4aaf0564833ed5
bpfam <joker1312boos@icloud.com>
2026-07-03 17:20:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bpfam-app-v1";
+const CACHE_NAME = "bpfam-app-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -12,6 +12,20 @@ const FILES_TO_CACHE = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
 
